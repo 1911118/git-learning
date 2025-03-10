c2ac0c7bad728399c2d4f0cfb748831c73cf8960
Rahiman <abdulrehamanshaik46@gmail.com>
2025-03-10 16:08:43 +0530

    Revert "add more junk food"
    This reverts commit c3ec17c004222263c4e48b461bfd97ebe3feb1d4.

--- testGit.py
+++ testGit.py
@@ -1,8 +1,5 @@
 hungry=input ( "are you hungry?" )
 if hungry.lower() == "yes":
     print("eat samosa")
-    print("eat pizza")
-    print("eat burger")
-    
 else :
     print("Do your homework")

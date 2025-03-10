2f6cf681a11226d7b73ccdbeb946c53be989afd6
Rahiman <abdulrehamanshaik46@gmail.com>
2025-03-10 16:55:08 +0530

    solid food added

--- testGit.py
+++ testGit.py
@@ -1,5 +1,9 @@
 hungry=input ( "are you hungry?" )
-if hungry.lower() == "yes":
+if hungry.lower() == "y":
     print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
+    print("eat pasta")
 else :
     print("Do your homework")

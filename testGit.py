c3ec17c004222263c4e48b461bfd97ebe3feb1d4
Rahiman <abdulrehamanshaik46@gmail.com>
2025-03-10 15:58:41 +0530

    add more junk food

--- testGit.py
+++ testGit.py
@@ -1,5 +1,8 @@
 hungry=input ( "are you hungry?" )
 if hungry.lower() == "yes":
     print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+    
 else :
     print("Do your homework")
